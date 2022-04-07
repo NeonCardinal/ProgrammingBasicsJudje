5eaba48d3dfc45571231007e0d0d1bf350331a9d
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-07 19:12:39 +0300

    add

---
+++ EXERCISE/RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class RadiansToDegrees
+{
+    static void Main()
+    {
+        double radians = double.Parse(Console.ReadLine());
+
+        double deg = radians * 180 / Math.PI;
+
+        Console.WriteLine(deg);
+    }
+}
