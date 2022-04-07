f8c87d9c5de34dfa375e836f874a9f10ac9ce766
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:50:25 +0300

    add

---
+++ MORE_EXERCISE/FirstStepsInCodingMoreExercises/Fishland/Fishland.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Fishland
+{
+    static void Main()
+    {
+        double priceForMackerel = double.Parse(Console.ReadLine());
+        double priceForSprat = double.Parse(Console.ReadLine());
+        double bonitoKillos = double.Parse(Console.ReadLine());
+        double scadKillos = double.Parse(Console.ReadLine());
+        double clamKillos = double.Parse(Console.ReadLine());
+
+        double bonitoPricePerKillo = priceForMackerel + priceForMackerel * 0.60;
+        double scadPricePerKillo = priceForSprat + priceForSprat * 0.80;
+
+        double priceForBonito = bonitoPricePerKillo * bonitoKillos;
+        double priceForScad = scadPricePerKillo * scadKillos;
+        double priceForClam = clamKillos * 7.50;
+
+        double totalMoney = priceForBonito + priceForScad + priceForClam;
+
+        Console.WriteLine($"{totalMoney:F2}");
+    }
+}
