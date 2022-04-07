5905bf11b45746057ae521e0853fad31aa83d17e
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:52:50 +0300

    add

---
+++ MORE_EXERCISE/FirstStepsInCodingMoreExercises/WeatherForecast/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class WeatherForecast
+{
+    static void Main()
+    {
+        string weather = Console.ReadLine().ToLower();
+
+        if (weather == "sunny")
+        {
+            Console.WriteLine("It's warm outside!");
+        }
+        else
+        {
+            Console.WriteLine("It's cold outside!");
+        }
+    }
+}
