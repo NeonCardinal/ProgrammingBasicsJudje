8204d875ee2ac052d8adc00a3e0d27a923d27591
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:49:55 +0300

    add

---
+++ MORE_EXERCISE/FirstStepsInCodingMoreExercises/CircleAreaAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class CircleAreaAndPerimeter
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+
+        double circleArea = Math.PI * r * r;
+        double circlePeremeter = 2 * Math.PI * r;
+
+        Console.WriteLine($"{circleArea:F2}");
+        Console.WriteLine($"{circlePeremeter:F2}");
+    }
+}
