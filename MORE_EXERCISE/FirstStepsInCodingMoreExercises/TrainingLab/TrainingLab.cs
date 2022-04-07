3c432f79f09087d6e95d9c54ad2ecda28207a651
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:51:14 +0300

    add

---
+++ MORE_EXERCISE/FirstStepsInCodingMoreExercises/TrainingLab/TrainingLab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class TrainingLab
+{
+    static void Main()
+    {
+        double w = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+
+        double widthInCm = w * 100;
+        double heightInCm = (h * 100) - 100;
+
+        int row = (int)(widthInCm / 120);
+        int col = (int)(heightInCm / 70);
+        int result = row * col - 3;
+
+        Console.WriteLine(result);
+    }
+}
