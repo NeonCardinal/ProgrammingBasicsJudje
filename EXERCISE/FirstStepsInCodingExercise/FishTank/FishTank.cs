c6770ae66926a39ffc65858a3015d736b5f0495b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:48:15 +0300

    add

---
+++ EXERCISE/FirstStepsInCodingExercise/FishTank/FishTank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class FishTank
+{
+    static void Main()
+    {
+        int len = int.Parse(Console.ReadLine());
+        int widht = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        double percent = double.Parse(Console.ReadLine());
+
+        double volume = (len * widht * height) * 0.001;
+        double takenArea = volume * (percent / 100.0);
+        double total = volume - takenArea;
+
+        Console.WriteLine(total);
+    }
+}
---
+++ EXERCISE/FirstStepsInCodingExercise/UsdToBgn/UsdToBgn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class UsdToBgn
+{
+    static void Main()
+    {
+        double usd = double.Parse(Console.ReadLine());
+
+        double bgn = usd * 1.79549;
+
+        Console.WriteLine(bgn);
+    }
+}
