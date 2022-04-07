6b4107594f1b7365b00f3bd2cb93e207c6f0f146
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:46:10 +0300

    add

---
+++ LAB/FirstStepsInCodingLab/RectangleArea/RectangleArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class RectangleArea
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int area = a * b;
+
+        Console.WriteLine(area);
+    }
+}
