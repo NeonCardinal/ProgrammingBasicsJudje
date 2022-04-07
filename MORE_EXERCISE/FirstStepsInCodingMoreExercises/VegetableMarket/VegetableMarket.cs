e6964a97b8174fa442c2fa8f2c788eaeb8088c85
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 00:52:24 +0300

    add

---
+++ MORE_EXERCISE/FirstStepsInCodingMoreExercises/VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class VegetableMarket
+{
+    static void Main()
+    {
+        double priceForVegetables = double.Parse(Console.ReadLine());
+        double priceForFruids = double.Parse(Console.ReadLine());
+        int vegetablesKillos = int.Parse(Console.ReadLine());
+        int fruidsKillos = int.Parse(Console.ReadLine());
+
+        double sum = priceForVegetables * vegetablesKillos + priceForFruids * fruidsKillos;
+        double sumInEuro = sum / 1.94;
+
+        Console.WriteLine($"{sumInEuro:F2}");
+    }
+}
