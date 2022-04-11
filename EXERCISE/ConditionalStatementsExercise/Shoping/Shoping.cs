6a81f7e1f77e5b72373f9e03c5c46c6ff1819f7f
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:31:37 +0300

    Add

---
+++ EXERCISE/ConditionalStatementsExercise/Shoping/Shoping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Shoping
+{
+    static void Main()
+    {
+        double budget = double.Parse(Console.ReadLine());
+        int vgaCounter = int.Parse(Console.ReadLine());
+        int cpuCounter = int.Parse(Console.ReadLine());
+        int ramCounter = int.Parse(Console.ReadLine());
+
+        double vgaCosts = vgaCounter * 250;
+        double cpuPrice = vgaCosts * 0.35;
+        double ramPrice = vgaCosts * 0.10;
+        double cpuCosts = cpuPrice * cpuCounter;
+        double ramCosts = ramPrice * ramCounter;
+        double totalCosts = vgaCosts + cpuCosts + ramCosts;
+        double discount = 0.0;
+
+        if(vgaCounter > cpuCounter)
+        {
+            discount = totalCosts * 0.15;
+            totalCosts -= discount;
+        }
+
+        if(totalCosts <= budget)
+        {
+            Console.WriteLine($"You have {(budget - totalCosts):F2} leva left!");
+        }
+        else
+        {
+            Console.WriteLine($"Not enough money! You need {(totalCosts - budget):F2} leva more!");
+        }
+    }
+}
