0fa42ec0116057ebb27dbececb0ba448d766440e
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:28:28 +0300

    Add

---
+++ LAB/ConditionalStatementsLab/GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        if(firstNumber >= secondNumber)
+        {
+            Console.WriteLine(firstNumber);
+        }
+        else
+        {
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
