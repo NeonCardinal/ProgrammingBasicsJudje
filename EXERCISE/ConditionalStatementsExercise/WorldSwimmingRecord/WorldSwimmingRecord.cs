3a8ed65b188e339b7c6ef368f8cd262f46cf2fa8
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:33:15 +0300

    Add

---
+++ EXERCISE/ConditionalStatementsExercise/WorldSwimmingRecord/WorldSwimmingRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class WorldSwimmingRecord
+{
+    static void Main()
+    {
+        decimal record = decimal.Parse(Console.ReadLine());
+        decimal destination = decimal.Parse(Console.ReadLine());
+        decimal timePerMeter = decimal.Parse(Console.ReadLine());
+
+        decimal time = destination * timePerMeter;
+        decimal addedSeconds = Math.Floor((destination / 15)) * 12.5m;
+        decimal totalTime = time + addedSeconds;
+
+        if(totalTime < record)
+        {
+            Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+        }
+        else
+        {
+            Console.WriteLine($"No, he failed! He was {(totalTime - record):F2} seconds slower.");
+        }
+    }
+}
