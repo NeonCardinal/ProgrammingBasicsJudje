05c5bb7208e34354632dded362990be8e8a19628
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:36:17 +0300

    Add

---
+++ MORE_EXERCISE/ConditionalStatementsMoreExercises/Pets/Pets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Pets
+{
+    static void Main()
+    {
+        int holydays = int.Parse(Console.ReadLine());
+        double food = double.Parse(Console.ReadLine());
+        double dogFoodPerDay = double.Parse(Console.ReadLine());
+        double catFoodPerday = double.Parse(Console.ReadLine());
+        double turtleFoodPerDay = double.Parse(Console.ReadLine());
+
+        double neededFood = dogFoodPerDay * holydays + catFoodPerday * holydays + ((turtleFoodPerDay * holydays) / 1000);
+
+        if(neededFood <= food)
+        {
+            Console.WriteLine($"{Math.Floor(food - neededFood)} kilos of food left.");
+        }
+        else
+        {
+            Console.WriteLine($"{Math.Ceiling(neededFood - food)} more kilos of food are needed.");
+        }
+    }
+}
