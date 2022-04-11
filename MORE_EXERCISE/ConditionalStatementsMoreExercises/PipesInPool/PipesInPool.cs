2887d6018fedc957b4c7195846fcce4c194e82b5
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:36:39 +0300

    Add

---
+++ MORE_EXERCISE/ConditionalStatementsMoreExercises/PipesInPool/PipesInPool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class PipesInPool
+{
+    static void Main()
+    {
+        int volume = int.Parse(Console.ReadLine());
+        int pipeOne = int.Parse(Console.ReadLine());
+        int pipeTwo = int.Parse(Console.ReadLine());
+        double hours = double.Parse(Console.ReadLine());
+
+        double water = (pipeOne + pipeTwo) * hours;
+
+        int volumePercent = (int)Math.Truncate(water / volume * 100);
+        int pipeOnePercent = (int)(pipeOne * hours / water * 100);
+        int pipeTwoPercent = (int)(pipeTwo * hours / water * 100);
+
+        if (water <= volume)
+        {
+            Console.WriteLine($"The pool is {volumePercent}% full. " +
+                $"Pipe 1: {pipeOnePercent}%. " +
+                $"Pipe 2: {pipeTwoPercent}%.");
+        }
+        else
+        {
+            Console.WriteLine($"For {hours} hours the pool overflows with {(water - volume)} liters.");
+        }
+    }
+}
