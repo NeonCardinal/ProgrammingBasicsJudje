2d31e6e4bc9b1ba437b9fd4321920d385c91ffba
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-11 23:28:49 +0300

    Add

---
+++ LAB/ConditionalStatementsLab/PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class PasswordGuess
+{
+    static void Main()
+    {
+        string password = "s3cr3t!P@ssw0rd";
+        string str = Console.ReadLine();
+
+        if(str == password)
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
