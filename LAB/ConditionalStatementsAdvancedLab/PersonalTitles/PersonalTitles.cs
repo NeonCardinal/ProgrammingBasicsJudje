b5e14142de3c6358b63dd96f7cec70afde848cb3
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:12:10 +0300

    Add

---
+++ LAB/ConditionalStatementsAdvancedLab/PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class PersonalTitles
+{
+    static void Main()
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine().ToLower();
+
+        if(gender == "m")
+        {
+            if(age < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            else if(age >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+        }
+        else if(gender == "f")
+        {
+            if(age < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+            else if(age >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
