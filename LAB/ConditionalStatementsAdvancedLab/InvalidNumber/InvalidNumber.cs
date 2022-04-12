37844fc650035b9c9e90b4357358e98648b58153
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:11:22 +0300

    Add

---
+++ LAB/ConditionalStatementsAdvancedLab/InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class InvalidNumber
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        bool isInvalid = (number < 100 || number > 200) && number != 0;
+
+        if(isInvalid)
+        {
+            Console.WriteLine("invalid");
+        }
+    }
+}
