1d3c986a6118e3b14c8355501a4326bdf21bb6b9
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:11:45 +0300

    Add

---
+++ LAB/ConditionalStatementsAdvancedLab/NumberInRange/NumberInRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class NumberInRange
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        bool isInRange = (number >= -100 && number <= 100) && number != 0;
+
+        if(isInRange)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
