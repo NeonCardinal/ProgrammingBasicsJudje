083d572e9831ccf8812ce44c12e525caa57e73f0
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:09:36 +0300

    Add

---
+++ LAB/ConditionalStatementsAdvancedLab/CinemaTicket/CinemaTicket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class CinemaTicket
+{
+    static void Main()
+    {
+        string day = Console.ReadLine();
+
+        if(day == "Monday")
+        {
+            Console.WriteLine(12);
+        }
+        else if(day == "Tuesday")
+        {
+            Console.WriteLine(12);
+        }
+        else if(day == "Wednesday")
+        {
+            Console.WriteLine(14);
+        }
+        else if(day == "Thursday")
+        {
+            Console.WriteLine(14);
+        }
+        else if(day == "Friday")
+        {
+            Console.WriteLine(12);
+        }
+        else if(day == "Saturday")
+        {
+            Console.WriteLine(16);
+        }
+        else if(day == "Sunday")
+        {
+            Console.WriteLine(16);
+        }
+    }
+}
