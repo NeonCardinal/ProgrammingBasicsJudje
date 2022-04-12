a17a2551d52f230fc553c0f5630cbec522ac9e70
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:14:00 +0300

    Add

---
+++ LAB/ConditionalStatementsAdvancedLab/WorkingHours/WorkingHours.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class WorkingHours
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+        bool isOpen = hours >= 10 && hours <= 18;
+
+        if(day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday")
+        {
+            if(isOpen)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+        else if(day == "Sunday")
+        {
+            Console.WriteLine("closed");
+        }
+    }
+}
