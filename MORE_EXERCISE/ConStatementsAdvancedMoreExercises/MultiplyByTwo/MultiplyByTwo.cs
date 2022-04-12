67cb29f9634a4a1b13566dc50f946262249ea7e3
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 01:25:02 +0300

    Add

---
+++ MORE_EXERCISE/ConStatementsAdvancedMoreExercises/MultiplyByTwo/MultiplyByTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MultiplyByTwo
+{
+    static void Main()
+    {
+        while(true)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if(number < 0)
+            {
+                Console.WriteLine("Negative number!");
+                break;
+            }
+
+            double result = number * 2;
+
+            Console.WriteLine($"Result: {result:F2}");
+        }
+    }
+}
