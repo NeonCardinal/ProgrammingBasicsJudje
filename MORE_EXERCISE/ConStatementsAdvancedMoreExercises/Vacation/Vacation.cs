e40f87e6d111cb87b0f48f9a853ff5a9ff062f8a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-13 23:37:14 +0300

    Add

---
+++ MORE_EXERCISE/ConStatementsAdvancedMoreExercises/Vacation/Vacation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Vacation
+{
+    static void Main()
+    {
+        double budget = double.Parse(Console.ReadLine());
+        string season = Console.ReadLine();
+
+        string place = string.Empty;
+        string location = string.Empty;
+        double price = 0.0;
+
+        if(budget <= 1000)
+        {
+            place = "Camp";
+
+            if(season == "Summer")
+            {
+                location = "Alaska";
+                price = budget * 0.65;
+            }
+            else if(season == "Winter")
+            {
+                location = "Morocco";
+                price = budget * 0.45;
+            }
+        }
+        else if(budget > 1000 && budget <= 3000)
+        {
+            place = "Hut";
+
+            if(season == "Summer")
+            {
+                location = "Alaska";
+                price = budget * 0.80;
+            }
+            else if(season == "Winter")
+            {
+                location = "Morocco";
+                price = budget * 0.60;
+            }
+        }
+        else if(budget > 3000)
+        {
+            place = "Hotel";
+
+            if(season == "Summer")
+            {
+                location = "Alaska";
+            }
+            else if(season == "Winter")
+            {
+                location = "Morocco";
+            }
+
+            price = budget * 0.90;
+        }
+
+        Console.WriteLine($"{location} - {place} - {price:F2}");
+    }
+}
