31dfa385bdc51fc8d9f6f7ec0dd3af2e8e757a22
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-15 19:50:32 +0300

    Add

---
+++ MORE_EXERCISE/ForLoopMoreExercise/ClockPartTwo/ClockPartTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ClockPartTwo
+{
+    static void Main()
+    {
+        for(int h = 0; h < 24; h++)
+        {
+            for(int m = 0; m < 60; m++)
+            {
+                for(int s = 0; s < 60; s++)
+                {
+                    Console.WriteLine($"{h} : {m} : {s}");
+                }
+            }
+        }
+    }
+}
