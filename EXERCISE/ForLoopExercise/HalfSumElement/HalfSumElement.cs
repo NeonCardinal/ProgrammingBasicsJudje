a29305208af279f47d3854e25fd29ee31c6a7e22
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-15 19:46:52 +0300

    Add

---
+++ EXERCISE/ForLoopExercise/HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class HalfSumElement
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int greater = int.MinValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            sum += number;
+
+            if(number > greater)
+            {
+                greater = number;
+            }
+        }
+
+        if((sum - greater) == greater)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine($"Sum = {greater}");
+        }
+        else
+        {
+            Console.WriteLine("No");
+            Console.WriteLine($"Diff = {Math.Abs(greater - (sum - greater))}");
+        }
+    }
+}
