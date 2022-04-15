80bb99dd180a3203def50239e3f0ad849446fdfa
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-15 19:50:48 +0300

    Add

---
+++ MORE_EXERCISE/ForLoopMoreExercise/EqualPairs/EqualPairs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class EqualPairs
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int prevSum = 0;
+        int diff = 0;
+        int maxDiff = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            sum += int.Parse(Console.ReadLine());
+            sum += int.Parse(Console.ReadLine());
+
+            if(i > 1)
+            {
+                diff = Math.Abs(sum - prevSum);
+            }
+
+            if(diff > maxDiff)
+            {
+                maxDiff = diff;
+            }
+
+            prevSum = sum;
+            sum = 0;
+        }
+
+        if(maxDiff == 0)
+        {
+            Console.WriteLine($"Yes, value={prevSum}");
+        }
+        else
+        {
+            Console.WriteLine($"No, maxdiff={maxDiff}");
+        }
+    }
+}
