2dcc6b7b8e7de159019a7ddf7ecabbda8f526701
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-15 19:44:29 +0300

    Add

---
+++ LAB/ForLoopLab/NumberSequence/NumberSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class NumberSequence
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int greater = int.MinValue;
+        int smaller = int.MaxValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > greater)
+            {
+                greater = number;
+            }
+            if(number < smaller)
+            {
+                smaller = number;
+            }
+        }
+
+        Console.WriteLine($"Max number: {greater}");
+        Console.WriteLine($"Min number: {smaller}");
+    }
+}
