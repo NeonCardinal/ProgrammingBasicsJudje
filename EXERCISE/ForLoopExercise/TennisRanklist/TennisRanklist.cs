8ab8dae126d3bf017b92eb366c795396a35fa5c8
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-15 19:48:16 +0300

    Add

---
+++ EXERCISE/ForLoopExercise/TennisRanklist/TennisRanklist.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class TennisRanklist
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int score = int.Parse(Console.ReadLine());
+
+        int addedScore = 0;
+        int wins = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            string str = Console.ReadLine();
+
+            if(str == "W")
+            {
+                addedScore += 2000;
+                wins++;
+            }
+            else if(str == "F")
+            {
+                addedScore += 1200;
+            }
+            else if(str == "SF")
+            {
+                addedScore += 720;
+            }
+        }
+
+        double averagePoints = addedScore * 1.0 / n;
+        double winsPercent = wins * 1.0 / n * 100;
+        double finalPoints = score + addedScore;
+
+        Console.WriteLine($"Final points: {finalPoints}");
+        Console.WriteLine($"Average points: {(Math.Floor(averagePoints))}");
+        Console.WriteLine($"{winsPercent:F2}%");
+    }
+}
