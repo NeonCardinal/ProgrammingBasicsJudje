73a79287e5e73f8464a38be0256713c99f53bd35
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:32:23 +0300

    Add

---
+++ EXERCISE/WhileLoopExercise/Cake/Cake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Cake
+{
+    static void Main()
+    {
+        int width = int.Parse(Console.ReadLine());
+        int len = int.Parse(Console.ReadLine());
+
+        int pieces = width * len;
+
+        while(pieces > 0)
+        {
+            string command = Console.ReadLine();
+
+            if(command == "STOP")
+            {
+                if(pieces > 0)
+                {
+                    Console.WriteLine($"{pieces} pieces are left.");
+                }
+                break;
+            }
+
+            int takenPieces = int.Parse(command);
+
+            pieces -= takenPieces;
+
+            if(pieces <= 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+                break;
+            }
+        }
+    }
+}
