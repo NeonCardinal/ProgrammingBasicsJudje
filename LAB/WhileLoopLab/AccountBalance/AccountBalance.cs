9f68f0de32e4f23089781a6e66fcce410add4293
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:29:03 +0300

    Add

---
+++ LAB/WhileLoopLab/AccountBalance/AccountBalance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class AccountBalance
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        double totalSum = 0.0;
+
+        while(input != "NoMoreMoney")
+        {
+            double sum = double.Parse(input);
+
+            if(sum <= 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+
+            Console.WriteLine($"Increase: {sum:F2}");
+            totalSum += sum;
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Total: {totalSum:F2}");
+    }
+}
