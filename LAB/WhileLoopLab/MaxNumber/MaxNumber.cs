33a96bc38cb91b196fa1a7b8e554cad2e950155d
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:29:46 +0300

    Add

---
+++ LAB/WhileLoopLab/MaxNumber/MaxNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MaxNumber
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        int greater = int.MinValue;
+
+        while(input != "Stop")
+        {
+            int number = int.Parse(input);
+
+            if(number > greater)
+            {
+                greater = number;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine(greater);
+    }
+}
