c36b833b9955bb4387daf6828629c053cc6e4aff
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:31:13 +0300

    Add

---
+++ LAB/WhileLoopLab/SequenceTwokPlusOne/SequenceTwokPlusOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class SequenceTwokPlusOne
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int k = 1;
+
+        while(k <= n)
+        {
+            Console.WriteLine(k);
+            k = k * 2 + 1;
+        }
+    }
+}
