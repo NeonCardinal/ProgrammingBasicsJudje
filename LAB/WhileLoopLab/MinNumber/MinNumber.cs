ce56497d1cef1942762d88ae898f3a915c41af4b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:30:07 +0300

    Add

---
+++ LAB/WhileLoopLab/MinNumber/MinNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MinNumber
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        int smaller = int.MaxValue;
+
+        while(input != "Stop")
+        {
+            int number = int.Parse(input);
+
+            if(number < smaller)
+            {
+                smaller = number;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine(smaller);
+    }
+}
