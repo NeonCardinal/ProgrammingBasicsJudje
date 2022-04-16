f0001049557ef08dc25b385d75e3a116dbf77489
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:32:42 +0300

    Add

---
+++ EXERCISE/WhileLoopExercise/Coins/Coins.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Coins
+{
+    static void Main()
+    {
+        decimal sum = decimal.Parse(Console.ReadLine());
+
+        int coins = 0;
+
+        while(true)
+        {
+            if (sum >= 2)
+            {
+                sum -= 2.0m;
+                coins++;
+            }
+            else if (sum >= 1 && sum < 2)
+            {
+                sum -= 1.0m;
+                coins++;
+            }
+            else if (sum >= 0.50m && sum < 1)
+            {
+                sum -= 0.50m;
+                coins++;
+            }
+            else if (sum >= 0.20m && sum < 0.50m)
+            {
+                sum -= 0.20m;
+                coins++;
+            }
+            else if (sum >= 0.10m && sum < 0.20m)
+            {
+                sum -= 0.10m;
+                coins++;
+            }
+            else if(sum >= 0.05m && sum < 0.10m)
+            {
+                sum -= 0.05m;
+                coins++;
+            }
+            else if(sum >= 0.02m && sum < 0.05m)
+            {
+                sum -= 0.02m;
+                coins++;
+            }
+            else if(sum >= 0.01m && sum < 0.02m)
+            {
+                sum -= 0.01m;
+                coins++;
+            }
+
+            if(sum == 0.0m)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine(coins);
+    }
+}
