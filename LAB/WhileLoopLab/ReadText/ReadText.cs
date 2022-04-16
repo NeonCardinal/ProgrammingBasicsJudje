d0591eaeb55e698fb2b09a9a2517218d4798c65c
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-16 12:30:50 +0300

    Add

---
+++ LAB/WhileLoopLab/ReadText/ReadText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class ReadText
+{
+    static void Main()
+    {
+
+        while(true)
+        {
+            string input = Console.ReadLine();
+
+            if(input == "Stop")
+            {
+                break;
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
