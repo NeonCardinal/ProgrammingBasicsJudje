ae10c7b9c5f9b9844a615182a17e586a6af9e24c
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-17 16:11:55 +0300

    Add

---
+++ MORE_EXERCISE/WhileLoopMoreExercise/AverageNumber/AverageNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class AverageNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double sum = 0.0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            sum += number;
+        }
+
+        Console.WriteLine($"{(sum / n):F2}");
+    }
+}
