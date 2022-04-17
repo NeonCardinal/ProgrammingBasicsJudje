f0ae7a4a3e718a3f17623c4f5f75e22b2651e996
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-17 16:13:43 +0300

    Add

---
+++ MORE_EXERCISE/WhileLoopMoreExercise/StreamOfLetters/StreamOfLetters.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class StreamOfLetters
+{
+    static void Main()
+    {
+        int c = 0;
+        int o = 0;
+        int n = 0;
+        string word = string.Empty;
+
+        while(true)
+        {
+            string symbol = Console.ReadLine();
+
+            if(symbol == "End")
+            {
+                break;
+            }
+
+            char ch = char.Parse(symbol);
+
+            if(char.IsLetter(ch))
+            {
+                if(ch == 'c')
+                {
+                    if(c > 0)
+                    {
+                        word += ch;
+                    }
+                    c++;
+                }
+                else if(ch == 'o')
+                {
+                    if(o > 0)
+                    {                   
+                        word += ch;
+                    }
+                    o++;
+                }
+                else if(ch == 'n')
+                {
+                    if(n > 0)
+                    {
+                        word += ch;
+                    }
+                    n++;
+                }
+                else
+                {
+                    word += ch;
+                }
+
+                if(c > 0 && o > 0 && n > 0)
+                {
+                    Console.Write($"{word} ");
+                    c = 0;
+                    o = 0;
+                    n = 0;
+                    word = string.Empty;
+                }
+            }
+        }
+    }
+}
