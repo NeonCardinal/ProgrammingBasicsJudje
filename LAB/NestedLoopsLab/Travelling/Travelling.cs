2df4290d2a66b04d54a2974e6f83fac5dc5ff0ba
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-21 23:44:36 +0300

    Add

---
+++ LAB/NestedLoopsLab/Travelling/Travelling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Travelling
+{
+    static void Main()
+    {
+        while(true)
+        {
+            string destination = Console.ReadLine();
+
+            if(destination == "End")
+            {
+                break;
+            }
+
+            double vacantionPrice = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            while(true)
+            {
+                double save = double.Parse(Console.ReadLine());
+
+                sum += save;
+
+                if(sum >= vacantionPrice)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                    break;
+                }
+            }
+        }
+    }
+}
