d0fd4e6a9ec8fbfd0a9aefaea3e5d5564c214def
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-22 13:06:43 +0300

    Add

---
+++ EXERCISE/NestedLoopsExercise/SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class SpecialNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    for (int p = 1; p <= 9; p++)
+                    {
+                        if (n % i == 0 && n % j == 0 && n % k == 0 && n % p == 0)
+                        {
+                            Console.Write($"{i}{j}{k}{p} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
