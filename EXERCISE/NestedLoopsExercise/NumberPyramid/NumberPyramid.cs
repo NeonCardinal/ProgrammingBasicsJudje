bb252ab00b89ef2a91faf6088d8060bfdb2e8661
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-22 13:06:27 +0300

    Add

---
+++ EXERCISE/NestedLoopsExercise/NumberPyramid/NumberPyramid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class NumberPyramid
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int currentNumber = 1;
+        bool isBigger = false;
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                if(currentNumber > n)
+                {
+                    break;
+                }
+
+                Console.Write($"{currentNumber} ");
+                currentNumber++;
+            }
+
+            if(isBigger)
+            {
+                break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
