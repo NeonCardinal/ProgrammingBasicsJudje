8d58f5d388c9989ff2c06ee9514c138719adfec6
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-22 13:07:19 +0300

    Add

---
+++ EXERCISE/NestedLoopsExercise/TrainTheTrainers/TrainTheTrainers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class TrainTheTrainers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int counter = 0;
+        double averageGrade = 0.0;
+
+        while(true)
+        {
+            double gradeSum = 0.0;
+            string str = Console.ReadLine();
+
+            if(str == "Finish")
+            {
+                break;
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                gradeSum += grade;
+                counter++;
+            }
+
+            Console.WriteLine($"{str} - {(gradeSum / n):F2}.");
+
+            averageGrade += gradeSum;
+        }
+
+        Console.WriteLine($"Student's final assessment is {(averageGrade / counter):F2}.");
+    }
+}
