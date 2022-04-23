6ec0beef61a9fc730b4a69d2f295369983605134
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-23 10:52:34 +0300

    Add

---
+++ MORE_EXERCISE/NestedLoopsMoreExercise/UniquePINCodes/UniquePINCodes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class UniquePINCodes
+{
+    static void Main()
+    {
+        int firstDigitMax = int.Parse(Console.ReadLine());
+        int secondDigitMax = int.Parse(Console.ReadLine());
+        int thirdDigitMax = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= firstDigitMax; i++)
+        {
+            for(int j = 1; j <= secondDigitMax; j++)
+            {
+                for(int k = 1; k <= thirdDigitMax; k++)
+                {
+                    if(i % 2 == 0 && (j == 2 || j == 3 || j == 5 ||
+                        j == 7) && k % 2 == 0)
+                    {
+                        Console.WriteLine($"{i} {j} {k}");
+                    }
+                }
+            }
+        }
+    }
+}
