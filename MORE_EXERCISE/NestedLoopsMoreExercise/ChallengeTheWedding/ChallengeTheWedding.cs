611dea2cdd78990b3bcfa0f1ce5ddb71c5285edc
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-23 10:51:32 +0300

    Add

---
+++ MORE_EXERCISE/NestedLoopsMoreExercise/ChallengeTheWedding/ChallengeTheWedding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class ChallengeTheWedding
+{
+    static void Main()
+    {
+        int menCounter = int.Parse(Console.ReadLine());
+        int womenCounter = int.Parse(Console.ReadLine());
+        int freeTables = int.Parse(Console.ReadLine());
+
+        int counter = 0;
+
+        for(int i = 1; i <= menCounter; i++)
+        {
+            for(int j = 1; j <= womenCounter; j++)
+            {
+                counter++;
+                Console.Write($"({i} <-> {j}) ");
+
+                if(counter == freeTables)
+                {
+                    break;
+                }
+            }
+
+            if (counter == freeTables)
+            {
+                break;
+            }
+        }
+    }
+}
