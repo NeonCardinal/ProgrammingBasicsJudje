d7a064067a4dc81cdefeac5acac9e56345b613e5
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-29 02:04:11 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/CatWalking/CatWalking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class CatWalking
+{
+    static void Main()
+    {
+        int minutes = int.Parse(Console.ReadLine());
+        int walkCounter = int.Parse(Console.ReadLine());
+        int calories = int.Parse(Console.ReadLine());
+
+        int minutesPerDay = minutes * walkCounter;
+        int burnedCalories = minutesPerDay * 5;
+        int needCaloriesToBurn = (int)(calories * 0.50);
+
+        if(burnedCalories >= needCaloriesToBurn)
+        {
+            Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnedCalories}.");
+        }
+        else
+        {
+            Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnedCalories}.");
+        }
+    }
+}
