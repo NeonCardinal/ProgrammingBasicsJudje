f4deb02d501e65abe3b506339278fc2430c92e85
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-29 02:03:48 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/BirthdayParty/BirthdayParty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class BirthdayParty
+{
+    static void Main()
+    {
+        double rent = double.Parse(Console.ReadLine());
+
+        double cakePrice = rent * 0.20;
+        double drinksPrice = cakePrice - (cakePrice * 0.45);
+        double animatorPrice = rent * 1 / 3;
+        double total = rent + cakePrice + drinksPrice + animatorPrice;
+
+        Console.WriteLine(total);
+    }
+}
