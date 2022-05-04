9f4f7a0d603b7212b3be594b8dc95530f6b6bb78
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 19:54:39 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/SuitcasesLoad/SuitcasesLoad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class SuitcasesLoad
+{
+    static void Main()
+    {
+        double volume = double.Parse(Console.ReadLine());
+        string command = Console.ReadLine();
+
+        int caseesCounter = 0;
+        double totalVolume = 0.0;
+
+        while(true)
+        {
+            if(command == "End")
+            {
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+                break;
+            }
+
+
+
+            double caseVolume = double.Parse(command);
+
+            if(caseesCounter % 3 == 0)
+            {
+                double add = caseVolume * 0.10;
+                caseVolume += add;
+            }
+
+            totalVolume += caseVolume;
+
+            if(totalVolume > volume)
+            {
+                Console.WriteLine($"No more space!");
+                break;
+            }
+
+            caseesCounter++;
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Statistic: {caseesCounter} suitcases loaded.");
+    }
+}
