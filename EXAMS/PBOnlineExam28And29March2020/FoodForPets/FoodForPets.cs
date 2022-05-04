b9c6d177825e465c6ff6f25a79d7c3dc132bf7ec
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 19:54:20 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/FoodForPets/FoodForPets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class FoodForPets
+{
+    static void Main()
+    {
+        int days = int.Parse(Console.ReadLine());
+        double food = double.Parse(Console.ReadLine());
+
+        int totalDogsFood = 0;
+        int totalCatsFood = 0;
+        int totalFood = 0;
+        int totalBisquits = 0;
+
+        for(int i = 1; i <= days; i++)
+        {
+            int dogsFood = int.Parse(Console.ReadLine());
+            int catsFood = int.Parse(Console.ReadLine());
+            int totalFoodPerDay = dogsFood + catsFood;
+
+            if(i % 3 == 0)
+            {
+                int bisquits = (int)Math.Round(totalFoodPerDay * 0.10);
+                totalBisquits = bisquits;
+            }
+
+
+            totalCatsFood += catsFood;
+            totalDogsFood += dogsFood;
+            totalFood += totalFoodPerDay;
+        }
+
+        double eatenFoodPercent = totalFood * 100.0 / food;
+        double catsFoodPercent = totalCatsFood * 100.0 / totalFood;
+        double dogsFoodPercent = totalDogsFood * 100.0 / totalFood;
+
+        Console.WriteLine($"Total eaten biscuits: {totalBisquits}gr.");
+        Console.WriteLine($"{eatenFoodPercent:F2}% of the food has been eaten.");
+        Console.WriteLine($"{dogsFoodPercent:F2}% eaten from the dog.");
+        Console.WriteLine($"{catsFoodPercent:F2}% eaten from the cat.");
+    }
+}
