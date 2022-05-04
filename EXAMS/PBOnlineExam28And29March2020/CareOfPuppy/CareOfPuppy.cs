3d0f98400ae0461302491f91cb5f3be636dfaef5
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 19:53:04 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/CareOfPuppy/CareOfPuppy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class CareOfPuppy
+{
+    static void Main()
+    {
+        int food = int.Parse(Console.ReadLine());
+        string command = Console.ReadLine();
+
+        int availableFoodInGrams = food * 1000;
+        int eatenFood = 0;
+
+        while(true)
+        {
+            if(command == "Adopted")
+            {
+                break;
+            }
+
+            int foodPerDay = int.Parse(command);
+            eatenFood += foodPerDay;
+
+            command = Console.ReadLine();
+        }
+
+        if(eatenFood <= availableFoodInGrams)
+        {
+            Console.WriteLine($"Food is enough! Leftovers: {availableFoodInGrams - eatenFood} grams.");
+        }
+        else
+        {
+            Console.WriteLine($"Food is not enough. You need {eatenFood - availableFoodInGrams} grams more.");
+        }
+    }
+}
