a2b845de50cebc40f9be83accfdbb21b11f37bc0
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 19:53:31 +0300

    Add

---
+++ EXAMS/PBOnlineExam28And29March2020/EnergyBooster/EnergyBooster.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+public class EnergyBooster
+{
+    static void Main()
+    {
+        string fruit = Console.ReadLine();
+        string type = Console.ReadLine();
+        int counts = int.Parse(Console.ReadLine());
+
+        double price = 0.0;
+        double pricePerSet = 0.0;
+        double total = 0.0;
+
+        if(fruit == "Watermelon")
+        {
+            if(type == "small")
+            {
+                pricePerSet = 2 * 56;
+                price = counts * pricePerSet;
+            }
+            else if(type == "big")
+            {
+                pricePerSet = 5 * 28.70;
+                price = counts * pricePerSet;
+            }
+        }
+        else if(fruit == "Mango")
+        {
+            if (type == "small")
+            {
+                pricePerSet = 2 * 36.66;
+                price = counts * pricePerSet;
+            }
+            else if (type == "big")
+            {
+                pricePerSet = 5 * 19.60;
+                price = counts * pricePerSet;
+            }
+        }
+        else if(fruit == "Pineapple")
+        {
+            if (type == "small")
+            {
+                pricePerSet = 2 * 42.10;
+                price = counts * pricePerSet;
+            }
+            else if (type == "big")
+            {
+                pricePerSet = 5 * 24.80;
+                price = counts * pricePerSet; 
+            }
+        }
+        else if(fruit == "Raspberry")
+        {
+            if (type == "small")
+            {
+                pricePerSet = 2 * 20;
+                price = counts * pricePerSet;
+            }
+            else if (type == "big")
+            {
+                pricePerSet = 5 * 15.20;
+                price = counts * pricePerSet;
+            }
+        }
+
+        if(price >= 400 && price <= 1000)
+        {
+            double discount = price * 0.15;
+            price -= discount;
+        }
+        else if(price > 1000)
+        {
+            double discount = price * 0.50;
+            price -= discount;
+        }
+
+        Console.WriteLine($"{price:F2} lv.");
+    }
+}
